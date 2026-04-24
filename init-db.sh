41450a129236410cda34ff3d1d84af1b778a65d0
amit-agrawal10 <amit.agrawal10@gmail.com>
2026-04-24 17:47:06 +0530

    chore: sync changes across services

--- init-db.sh
+++ init-db.sh
@@ -6,4 +6,7 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     SELECT 'CREATE DATABASE smarttrade_broker_adapter_service' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'smarttrade_broker_adapter_service')\gexec
     SELECT 'CREATE DATABASE smarttrade_market_data_service' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'smarttrade_market_data_service')\gexec
     SELECT 'CREATE DATABASE smarttrade_paper_broker_service' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'smarttrade_paper_broker_service')\gexec
+    SELECT 'CREATE DATABASE smarttrade_strategy_service' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'smarttrade_strategy_service')\gexec
+    SELECT 'CREATE DATABASE smarttrade_journal_service' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'smarttrade_journal_service')\gexec
+    SELECT 'CREATE DATABASE smarttrade_portfolio_service' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'smarttrade_portfolio_service')\gexec
 EOSQL
